2796805ac2a99ac26671960d168b4a62dc125e7e
bhshimab <bshimabukuro@ucdavis.edu>
2022-05-02 09:18:47 -0700

    Added key timeout

--- src/hawkeye_steering/robot_steering.py
+++ src/hawkeye_steering/robot_steering.py
@@ -4,7 +4,7 @@ import rospkg
 import rospy
 
 from geometry_msgs.msg import Twist
-from python_qt_binding import loadUi
+from python_qt_binding import loadUi, QtCore
 from python_qt_binding.QtCore import Qt, QTimer, Slot
 from python_qt_binding.QtGui import QKeySequence
 from python_qt_binding.QtWidgets import QShortcut, QWidget
@@ -33,24 +33,30 @@ class HawkeyeSteering(Plugin):
 
 	self.twist = Twist()
 
+	# Define WASD keybinds and behavior
 	self.shortcut_w = QShortcut(QKeySequence(Qt.Key_W), self._widget)
         self.shortcut_w.setContext(Qt.ApplicationShortcut)
         self.shortcut_w.activated.connect(self.up_pushed)
+	self.shortcut_w.activated.connect(self.button_pressed)
         self.shortcut_s = QShortcut(QKeySequence(Qt.Key_S), self._widget)
         self.shortcut_s.setContext(Qt.ApplicationShortcut)
         self.shortcut_s.activated.connect(self.down_pushed)
+	self.shortcut_s.activated.connect(self.button_pressed)
         self.shortcut_a = QShortcut(QKeySequence(Qt.Key_A), self._widget)
         self.shortcut_a.setContext(Qt.ApplicationShortcut)
         self.shortcut_a.activated.connect(self.left_pushed)
+	self.shortcut_a.activated.connect(self.button_pressed)
         self.shortcut_d = QShortcut(QKeySequence(Qt.Key_D), self._widget)
         self.shortcut_d.setContext(Qt.ApplicationShortcut)
         self.shortcut_d.activated.connect(self.right_pushed)
+	self.shortcut_d.activated.connect(self.button_pressed)
 
-        self._update_parameter_timer = QTimer(self)
-        self._update_parameter_timer.timeout.connect(
-            self._send_zero)
-        self._update_parameter_timer.start(100)
+	# Auto timeout and zero pulse sent
+	self.flag = 0
         self.zero_cmd_sent = False
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self._send_zero)
+        self.timer.start(50)
 
         # Sliders and gui buttons
         self._widget.slider_linear_speed.valueChanged.connect(
@@ -68,14 +74,19 @@ class HawkeyeSteering(Plugin):
         self._widget.button_angular_decrease.pressed.connect(
             self._on_decrease_z_angular_pressed)
 
+	# Display the set speed next to the speed sliders
         self._widget.label_linear_speed.setText(
             'Linear speed:\n%0.3f m/s' % (self._widget.slider_linear_speed.value() / 200))
-
         self._widget.label_angular_speed.setText(
             'Angular speed: %0.3f rad/s' % (self._widget.slider_angular_speed.value() / 200))
 
+    def button_pressed(self):
+	self.flag = 12
+
     def _send_zero(self):
-        if not self.zero_cmd_sent:
+	if self.flag > 0:
+	    self.zero_cmd_sent = True
+	elif self.flag == 0:
 	    self.twist.linear.x = 0
 	    self.twist.linear.y = 0
 	    self.twist.linear.z = 0
@@ -84,9 +95,23 @@ class HawkeyeSteering(Plugin):
 	    self.twist.angular.z = 0
             self.zero_cmd_sent = True
             self._publisher.publish(self.twist)
+	    self.zero_cmd_sent = False
+
+ 	if self.flag > -1:
+	    self.flag = self.flag - 1
+
+  #      if not self.zero_cmd_sent:
+#	    self.twist.linear.x = 0
+#	    self.twist.linear.y = 0
+#	    self.twist.linear.z = 0
+#	    self.twist.angular.x = 0
+#	    self.twist.angular.y = 0
+#	    self.twist.angular.z = 0
+ #           self.zero_cmd_sent = True
+  #          self._publisher.publish(self.twist)
 
     # Button press definitions
-    def up_pushed(self, checked = False):
+    def up_pushed(self):
 	self.twist.linear.x = 1 * self._widget.slider_linear_speed.value() / 200
 	self.twist.linear.y = 0
 	self.twist.linear.z = 0
@@ -95,7 +120,7 @@ class HawkeyeSteering(Plugin):
 	self.twist.angular.z = 0
         self._publisher.publish(self.twist)
 
-    def down_pushed(self, checked = False):
+    def down_pushed(self):
 	self.twist.linear.x = -1 * self._widget.slider_linear_speed.value() / 200
 	self.twist.linear.y = 0
 	self.twist.linear.z = 0
@@ -104,16 +129,7 @@ class HawkeyeSteering(Plugin):
 	self.twist.angular.z = 0
         self._publisher.publish(self.twist)
 
-    def left_pushed(self, checked = False):
-	self.twist.linear.x = 0
-	self.twist.linear.y = 0
-	self.twist.linear.z = 0
-	self.twist.angular.x = 0
-	self.twist.angular.y = 0
-	self.twist.angular.z = 1 * self._widget.slider_angular_speed.value() / 200
-        self._publisher.publish(self.twist)
-
-    def right_pushed(self, checked = False):
+    def left_pushed(self):
 	self.twist.linear.x = 0
 	self.twist.linear.y = 0
 	self.twist.linear.z = 0
@@ -122,7 +138,16 @@ class HawkeyeSteering(Plugin):
 	self.twist.angular.z = -1 * self._widget.slider_angular_speed.value() / 200
         self._publisher.publish(self.twist)
 
-    def arrow_released(self, checked = False):
+    def right_pushed(self):
+	self.twist.linear.x = 0
+	self.twist.linear.y = 0
+	self.twist.linear.z = 0
+	self.twist.angular.x = 0
+	self.twist.angular.y = 0
+	self.twist.angular.z = 1 * self._widget.slider_angular_speed.value() / 200
+        self._publisher.publish(self.twist)
+
+    def arrow_released(self):
 
 	self.twist.linear.x = 0
 	self.twist.linear.y = 0
@@ -156,3 +181,9 @@ class HawkeyeSteering(Plugin):
     def _on_decrease_z_angular_pressed(self):
         self._widget.slider_angular_speed.setValue(
             self._widget.slider_angular_speed.value() - self._widget.slider_angular_speed.singleStep())
+
+    def _on_key_release(self):
+        if event.key() == QtCore.Qt.Key_Control:
+            self._send_zero()
+            self.is_key_ctrl_pressed = False
+        super(Dialog, self).keyReleaseEvent(event)
